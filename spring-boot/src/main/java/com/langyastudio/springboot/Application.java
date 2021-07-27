64b8fae158af81f62d783769b4ac0a750b5195b7
langyastudio <1032030048@qq.com>
2021-07-27 14:32:49 +0800

    spring boot init

---
+++ spring-boot/src/main/java/com/langyastudio/springboot/Application.java
@@ -0,0 +1,16 @@
+package com.langyastudio.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 应用程序入口
+ */
+@SpringBootApplication
+public class Application
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Application.class, args);
+    }
+}
