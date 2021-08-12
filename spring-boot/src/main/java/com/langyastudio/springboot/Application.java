22da3515a1581afbba979e6e0f1a6e64952ae742
langyastudio <1032030048@qq.com>
2021-08-12 14:41:18 +0800

    no message

--- spring-boot/src/main/java/com/langyastudio/springboot/Application.java
+++ spring-boot/src/main/java/com/langyastudio/springboot/Application.java
@@ -2,15 +2,22 @@ package com.langyastudio.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.admin.SpringApplicationAdminJmxAutoConfiguration;
 
 /**
  * 应用程序入口
  */
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.langyastudio.springboot"},
+exclude = SpringApplicationAdminJmxAutoConfiguration.class)
 public class Application
 {
     public static void main(String[] args)
     {
-        SpringApplication.run(Application.class, args);
+        SpringApplication app = new SpringApplication(Application.class);
+
+        //app.setBannerMode(Banner.Mode.OFF);
+        //app.addListeners(new MyListener());
+
+        app.run(args);
     }
 }
