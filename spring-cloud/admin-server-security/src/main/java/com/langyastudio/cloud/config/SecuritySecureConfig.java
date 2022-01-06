cb9525832fae0193d485843494d84702e3788c61
langyastudio <1032030048@qq.com>
2022-01-06 17:33:28 +0800

    add spring-boot-admin Customizing

--- spring-cloud/admin-server-security/src/main/java/com/langyastudio/cloud/config/SecuritySecureConfig.java
+++ spring-cloud/admin-server-security/src/main/java/com/langyastudio/cloud/config/SecuritySecureConfig.java
@@ -3,7 +3,6 @@ package com.langyastudio.cloud.config;
 import de.codecentric.boot.admin.server.config.AdminServerProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
