9de00489ed440b83703bff5ba65ecc04911137bb
langyastudio <jiangjiaxiong@hacfin.com>
2022-01-10 22:08:20 +0800

    temp

--- spring-cloud/sentinel-server/src/main/java/com/langyastudio/cloud/config/FeignConfig.java
+++ spring-cloud/sentinel-server/src/main/java/com/langyastudio/cloud/config/FeignConfig.java
@@ -3,18 +3,11 @@ package com.langyastudio.cloud.config;
 import com.langyastudio.cloud.exception.OpenFeignErrorDecoder;
 import com.langyastudio.cloud.service.impl.EchoServiceImpl;
 import feign.Logger;
-import feign.RequestInterceptor;
 import feign.Retryer;
 import feign.codec.ErrorDecoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.Enumeration;
-import java.util.LinkedHashMap;
-import java.util.Map;
 
 @Configuration
 public class FeignConfig
